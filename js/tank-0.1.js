986676c80bcb1be3b5d83ebafa7213040a33aa04
Matthew Daines <spuddermax@gmail.com>
2023-04-12 13:30:21 -0600

    Improve keyListener function

--- js/tank-0.1.js
+++ js/tank-0.1.js
@@ -572,9 +572,14 @@ function setStats(tank_obj) {
 	$(`#${tank_obj.id}_angle`).html(`${tank_obj.angle}`);
 }
 
+// Cache frequently accessed elements
+const tankProfile0 = $(".tank_0, .tank_0_ghost");
+const tankProfile1 = $(".tank_1, .tank_1_ghost");
+
 function keyListener(e) {
 	// If playStatus is false and key is not 32, return
-	if (!playStatus && e.keyCode != 32) return;
+	if (!playStatus && e.keyCode !== 32 && e.keyCode !== 73 && e.keyCode !== 71)
+		return;
 
 	const validKeys = {
 		73: () => {
@@ -584,40 +589,26 @@ function keyListener(e) {
 				: $("#instructions").animate({ height: "200px" }, 500);
 		},
 		32: () => {
-			playStatus ? stop_game() : start_game();
+			playStatus ? stopGame() : startGame();
 			$(".paused").toggle();
 		},
 		38: () => {
-			//console.log("forward 0");
 			if (tagTimer > 0 && tanks[0].tagged) return;
 			tanks[0].lastDriveTime = Date.now();
-			if (tanks[0].speed !== 0) {
-				// console.log("engine stop");
-				queueEngineStop(tanks[0]);
-			}
+			if (tanks[0].speed !== 0) queueEngineStop(tanks[0]);
 			tanks[0].speed++;
 			tanks[0].speed = Math.min(1, tanks[0].speed);
 			queueEngineStart(tanks[0]);
 		},
 		40: () => {
-			// console.log("back 0");
 			if (tagTimer > 0 && tanks[0].tagged) return;
 			tanks[0].lastDriveTime = Date.now();
 			tanks[0].speed--;
-
-			if (tanks[0].speed === 0) {
-				// console.log("engine stop");
-				queueEngineStop(tanks[0]);
-			}
-			if (tanks[0].speed !== 0) {
-				// console.log("engine start");
-				queueEngineStart(tanks[0]);
-			}
-
+			if (tanks[0].speed === 0) queueEngineStop(tanks[0]);
+			if (tanks[0].speed !== 0) queueEngineStart(tanks[0]);
 			tanks[0].speed = Math.max(-1, tanks[0].speed);
 		},
 		39: () => {
-			// console.log("turn right 0");
 			tanks[0].angle = (tanks[0].angle + 45) % 360;
 			$(".tank_0, .tank_0_ghost").css(
 				"transform",
@@ -626,7 +617,6 @@ function keyListener(e) {
 			setStats(tanks[0]);
 		},
 		37: () => {
-			// console.log("turn left 0");
 			tanks[0].angle = (tanks[0].angle - 45 + 360) % 360;
 			$(".tank_0, .tank_0_ghost").css(
 				"transform",
@@ -635,34 +625,24 @@ function keyListener(e) {
 			setStats(tanks[0]);
 		},
 		87: () => {
-			// console.log("forward 1");
 			if (tagTimer > 0 && tanks[1].tagged) return;
 			tanks[1].lastDriveTime = Date.now();
 			if (tanks[1].speed !== 0) {
-				// console.log("engine stop");
 				queueEngineStop(tanks[1]);
 			}
-			tanks[1].speed++;
-			tanks[1].speed = Math.min(1, tanks[1].speed);
+			tanks[1].speed = Math.min(1, tanks[1].speed + 1);
 			queueEngineStart(tanks[1]);
 		},
 		83: () => {
-			// console.log("back 1");
 			if (tagTimer > 0 && tanks[1].tagged) return;
-			tanks[1].speed--;
+			tanks[1].speed = Math.max(-1, tanks[1].speed - 1);
 			if (tanks[1].speed === 0) {
-				// console.log("engine stop");
 				queueEngineStop(tanks[1]);
-			}
-			if (tanks[1].speed !== 0) {
-				// console.log("engine start");
+			} else {
 				queueEngineStart(tanks[1]);
 			}
-
-			tanks[1].speed = Math.max(-1, tanks[1].speed);
 		},
 		68: () => {
-			// console.log("turn right 1");
 			tanks[1].angle = (tanks[1].angle + 45) % 360;
 			$(".tank_1, .tank_1_ghost").css(
 				"transform",
@@ -680,17 +660,135 @@ function keyListener(e) {
 			setStats(tanks[1]);
 		},
 		71: () => {
-			// console.log("toggle ghosts");
 			$(".tank_0_ghost, .tank_1_ghost").toggle();
 		},
 	};
-
 	const func = validKeys[e.keyCode];
 	if (func) {
 		func();
 		validKey = true;
 	}
 }
+
+// function keyListener(e) {
+// 	// If playStatus is false and key is not 32, return
+// 	if (!playStatus && e.keyCode != 32) return;
+
+// 	const validKeys = {
+// 		73: () => {
+// 			var current_height = $("#instructions").height();
+// 			current_height === 200
+// 				? $("#instructions").animate({ height: "20px" }, 500)
+// 				: $("#instructions").animate({ height: "200px" }, 500);
+// 		},
+// 		32: () => {
+// 			playStatus ? stopGame() : startGame();
+// 			$(".paused").toggle();
+// 		},
+// 		38: () => {
+// 			//console.log("forward 0");
+// 			if (tagTimer > 0 && tanks[0].tagged) return;
+// 			tanks[0].lastDriveTime = Date.now();
+// 			if (tanks[0].speed !== 0) {
+// 				// console.log("engine stop");
+// 				queueEngineStop(tanks[0]);
+// 			}
+// 			tanks[0].speed++;
+// 			tanks[0].speed = Math.min(1, tanks[0].speed);
+// 			queueEngineStart(tanks[0]);
+// 		},
+// 		40: () => {
+// 			// console.log("back 0");
+// 			if (tagTimer > 0 && tanks[0].tagged) return;
+// 			tanks[0].lastDriveTime = Date.now();
+// 			tanks[0].speed--;
+
+// 			if (tanks[0].speed === 0) {
+// 				// console.log("engine stop");
+// 				queueEngineStop(tanks[0]);
+// 			}
+// 			if (tanks[0].speed !== 0) {
+// 				// console.log("engine start");
+// 				queueEngineStart(tanks[0]);
+// 			}
+
+// 			tanks[0].speed = Math.max(-1, tanks[0].speed);
+// 		},
+// 		39: () => {
+// 			// console.log("turn right 0");
+// 			tanks[0].angle = (tanks[0].angle + 45) % 360;
+// 			$(".tank_0, .tank_0_ghost").css(
+// 				"transform",
+// 				`rotate(${tanks[0].angle}deg)`
+// 			);
+// 			setStats(tanks[0]);
+// 		},
+// 		37: () => {
+// 			// console.log("turn left 0");
+// 			tanks[0].angle = (tanks[0].angle - 45 + 360) % 360;
+// 			$(".tank_0, .tank_0_ghost").css(
+// 				"transform",
+// 				`rotate(${tanks[0].angle}deg)`
+// 			);
+// 			setStats(tanks[0]);
+// 		},
+// 		87: () => {
+// 			// console.log("forward 1");
+// 			if (tagTimer > 0 && tanks[1].tagged) return;
+// 			tanks[1].lastDriveTime = Date.now();
+// 			if (tanks[1].speed !== 0) {
+// 				// console.log("engine stop");
+// 				queueEngineStop(tanks[1]);
+// 			}
+// 			tanks[1].speed++;
+// 			tanks[1].speed = Math.min(1, tanks[1].speed);
+// 			queueEngineStart(tanks[1]);
+// 		},
+// 		83: () => {
+// 			// console.log("back 1");
+// 			if (tagTimer > 0 && tanks[1].tagged) return;
+// 			tanks[1].speed--;
+// 			if (tanks[1].speed === 0) {
+// 				// console.log("engine stop");
+// 				queueEngineStop(tanks[1]);
+// 			}
+// 			if (tanks[1].speed !== 0) {
+// 				// console.log("engine start");
+// 				queueEngineStart(tanks[1]);
+// 			}
+
+// 			tanks[1].speed = Math.max(-1, tanks[1].speed);
+// 		},
+// 		68: () => {
+// 			// console.log("turn right 1");
+// 			tanks[1].angle = (tanks[1].angle + 45) % 360;
+// 			$(".tank_1, .tank_1_ghost").css(
+// 				"transform",
+// 				`rotate(${tanks[1].angle}deg)`
+// 			);
+// 			setStats(tanks[1]);
+// 		},
+// 		65: () => {
+// 			// console.log("turn left 1");
+// 			tanks[1].angle = (tanks[1].angle - 45 + 360) % 360;
+// 			$(".tank_1, .tank_1_ghost").css(
+// 				"transform",
+// 				`rotate(${tanks[1].angle}deg)`
+// 			);
+// 			setStats(tanks[1]);
+// 		},
+// 		71: () => {
+// 			// console.log("toggle ghosts");
+// 			$(".tank_0_ghost, .tank_1_ghost").toggle();
+// 		},
+// 	};
+
+// 	const func = validKeys[e.keyCode];
+// 	if (func) {
+// 		func();
+// 		validKey = true;
+// 	}
+// }
 
 // Loop the game
 function play_game() {
@@ -714,12 +812,12 @@ function play_game() {
 }
 
 // Pause the game
-function stop_game() {
+function stopGame() {
 	playStatus = false;
 }
 
 // Start the game
-function start_game() {
+function startGame() {
 	playStatus = true;
 	play_game();
 }
